80419f8b3e987f2124b2fb5b21fa843f48ce517c
kon_yu <yusukedrive@gmail.com>
2026-06-03 13:22:38 +0900

    Suppress Qt font database OpenType warnings (qt.text.font.db script 11 = Gurmukhi, harmless)

--- kikitori/ui_pyside.py
+++ kikitori/ui_pyside.py
@@ -1,6 +1,8 @@
 """PySide6 ベースの Kikitori UI（メニューバー + オーバーレイ）"""
 
 import os
+os.environ.setdefault("QT_LOGGING_RULES", "qt.text.font.db.warning=false")
+
 import signal
 import sys
 import threading
--- pyside_main.py
+++ pyside_main.py
@@ -3,6 +3,9 @@
 Kikitori - PySide6 版エントリーポイント
 画面中央下に音声入力中オーバーレイUIを表示
 """
+import os
+os.environ["QT_LOGGING_RULES"] = "qt.text.font.db.warning=false"
+
 from kikitori.ui_pyside import main
 
 if __name__ == "__main__":
