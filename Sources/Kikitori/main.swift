f03dd7949286b9e42f703a9833ae5636424b4a04
kon_yu <yusukedrive@gmail.com>
2026-06-14 00:45:06 +0900

    feat: Homebrew Formula #16 - Swift build workflow + --version flag (64 tests pass)

--- Sources/Kikitori/main.swift
+++ Sources/Kikitori/main.swift
@@ -1,4 +1,11 @@
 import AppKit
+
+let args = CommandLine.arguments
+if args.contains("--version") || args.contains("-v") {
+    print("Kikitori 0.8.0")
+    exit(0)
+}
+
 let a = NSApplication.shared
 a.setActivationPolicy(.accessory)
 a.delegate = AppDelegate()
